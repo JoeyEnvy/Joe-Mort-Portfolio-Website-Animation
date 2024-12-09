b92228502664f258d834b38e7abcec7fe857e232
Your Name <c.fear.907@gmail.com>
2024-12-09 17:27:18 +0000

    efrgerg

--- aboutScript.js
+++ aboutScript.js
@@ -407,6 +407,20 @@ document.addEventListener('DOMContentLoaded', function() {
     observer.observe(splitContainer);
 });
 
+
+//navbar scroll effect 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
     // =========================================
     // Error Handling
     // =========================================
